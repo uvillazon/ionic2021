1e7f39c364b5e18dcd5d9ef1429e104b7c40a9c7
Ubaldo Villazon <uvillazon@gmail.com>
2021-01-20 19:16:34 -0400

    mejoras

--- src/app/paginas/inicio/inicio.page.ts
+++ src/app/paginas/inicio/inicio.page.ts
@@ -13,6 +13,35 @@ interface Componente {
 })
 export class InicioPage implements OnInit {
 
+  componente:Componente[]=[
+    {
+      icono: 'person-outline',
+      nombre:'Action Sheet1',
+      direccion:'/action-sheet',
+    },
+    {
+      icono: 'pricetag-outline',
+      nombre:'Alert1',
+      direccion:'/alert',
+    },
+    {
+      icono: 'planet-outline',
+      nombre:'Fab1',
+      direccion:'/fab',
+    },
+    {
+      icono: 'rocket-outline',
+      nombre:'Date Time1',
+      direccion:'/datetime',
+    },
+    {
+      icono: 'albums-outline',
+      nombre:'Botones1',
+      direccion:'/buttons',
+    },
+   
+  ]
+
   constructor() { }
 
   ngOnInit() {
