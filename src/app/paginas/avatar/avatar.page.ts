99eb690c444f4015995aae55f672acb0f8b77fff
Ubaldo Villazon <uvillazon@gmail.com>
2021-01-20 21:10:39 -0400

    mejoras en los botton

--- src/app/paginas/avatar/avatar.page.ts
+++ src/app/paginas/avatar/avatar.page.ts
@@ -11,20 +11,40 @@ interface Avatar {
   styleUrls: ['./avatar.page.scss'],
 })
 export class AvatarPage implements OnInit {
-  
+  repetir = Array(100);
   avatar:Avatar[]=[
     {
-      icono: 'imagen1',
-      nombre:'Action Sheet1'
+      icono: 'assets/img/imagen1.jpg',
+      nombre:'Naruto1'
     },
     {
-      icono: 'pricetag-outline',
-      nombre:'Alert1',
+      icono: 'assets/img/imagen2.jpg',
+      nombre:'Naruto2'
     },
     {
-      icono: 'planet-outline',
-      nombre:'Fab1'
-    },  
+      icono: 'assets/img/imagen3.jpg',
+      nombre:'Naruto3'
+    },
+    {
+      icono: 'assets/img/imagen4.jpg',
+      nombre:'Naruto4'
+    },
+    {
+      icono: 'assets/img/imagen5.jpg',
+      nombre:'Naruto5'
+    },
+    {
+      icono: 'assets/img/imagen4.jpg',
+      nombre:'Naruto4'
+    },
+    {
+      icono: 'assets/img/imagen3.jpg',
+      nombre:'Naruto3'
+    },
+    {
+      icono: 'assets/img/imagen2.jpg',
+      nombre:'Naruto2'
+    },
   ]
 
   constructor() { }
