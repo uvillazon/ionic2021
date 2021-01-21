d2ef73bfa6c2937baa27500a414ca1717d15c744
Ubaldo Villazon <uvillazon@gmail.com>
2021-01-20 20:39:08 -0400

    fab

--- src/app/paginas/avatar/avatar.page.ts
+++ src/app/paginas/avatar/avatar.page.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-interface Componente {
+interface Avatar {
   icono:string ;
   nombre:string ;
-  direccion:string;
 }
 
 @Component({
@@ -13,38 +12,19 @@ interface Componente {
 })
 export class AvatarPage implements OnInit {
   
-  avatar:Componente[]=[
+  avatar:Avatar[]=[
     {
-      icono: 'person-outline',
-      nombre:'Action Sheet1',
-      direccion:'/action-sheet',
+      icono: 'imagen1',
+      nombre:'Action Sheet1'
     },
     {
       icono: 'pricetag-outline',
       nombre:'Alert1',
-      direccion:'/alert',
     },
     {
       icono: 'planet-outline',
-      nombre:'Fab1',
-      direccion:'/fab',
-    },
-    {
-      icono: 'rocket-outline',
-      nombre:'Date Time1',
-      direccion:'/datetime',
-    },
-    {
-      icono: 'albums-outline',
-      nombre:'Botones1',
-      direccion:'/buttons',
-    },
-    {
-      icono: 'albums-outline',
-      nombre:'Avatar',
-      direccion:'/avatar',
-    },
-   
+      nombre:'Fab1'
+    },  
   ]
 
   constructor() { }
